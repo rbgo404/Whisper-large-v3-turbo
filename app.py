fe4403e3534932dd24cb21fff711f1552e302254
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-10-09 08:11:37 +0000

    added OpenAI whisper

---
+++ app.py
@@ -0,0 +1,30 @@
+class InferlessPythonModel:
+        
+    def initialize(self):
+        model_id = "openai/whisper-large-v3-turbo"
+        model = AutoModelForSpeechSeq2Seq.from_pretrained(
+            model_id,
+            torch_dtype=torch.float16,
+            low_cpu_mem_usage=True,
+            use_safetensors=True,
+            device_map="cuda"
+        )
+        
+        processor = AutoProcessor.from_pretrained(model_id)
+        
+        self.pipe = pipeline(
+            "automatic-speech-recognition",
+            model=model,
+            tokenizer=processor.tokenizer,
+            feature_extractor=processor.feature_extractor,
+            torch_dtype=torch.float16,
+            return_timestamps=True
+        )
+
+    def infer(self, inputs):
+        audio_url = inputs["audio_url"]
+        result = self.pipe(audio_url)
+        print(result["text"])
+
+    def finalize(self):
+        self.pipe = None
